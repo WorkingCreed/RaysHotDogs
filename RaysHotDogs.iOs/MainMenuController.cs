a17b0de94335b50e959e400fff890046ce75c5f2
cnreed <carolnreed@gmail.com>
2017-09-05 14:28:14 -0700

    Adding MainMenuController

---
+++ RaysHotDogs.iOs/MainMenuController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace RaysHotDogs.iOs
+{
+    public partial class MainMenuController : UIViewController
+    {
+        public MainMenuController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
